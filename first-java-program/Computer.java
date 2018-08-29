41582448ff97d0cbeadba6781a1c66335f0b4059
Malikatim <mmtimirova@gmail.com>
2018-08-29 16:17:53 -0500

    java tutorial

---
+++ first-java-program/Computer.java
@@ -0,0 +1,7 @@
+public class Computer{
+	public static void main(String[] args){
+	System.out.println("My computer is IMac");	
+System.out.println("My computer has MacOS High Sierra OS");
+System.out.println("My computer has 16 GB of RAM");
+	}
+}						
---
+++ first-java-program/MyName.java
@@ -0,0 +1,7 @@
+public class MyName{
+	public static void main(String[] args){
+System.out.println(1);
+String name = "Malika";
+System.out.println(name);
+	}
+}
