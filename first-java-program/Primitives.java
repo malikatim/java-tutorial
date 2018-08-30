3f2702010bfb67c22771e53a5688869f9ce261f6
Malikatim <mmtimirova@gmail.com>
2018-08-29 19:56:05 -0500

    add Primitives

---
+++ first-java-program/Primitives.java
@@ -0,0 +1,31 @@
+public class Primitives{
+	public static void main(String[] args){
+
+byte b = 123;
+byte bt = -56;
+
+short s = 12335;
+short sh = -2344;
+
+int i = 1343545;
+int in = -3455;
+
+long l = 4849383989333l;
+long ln = -443232243l;
+
+float f = 5.43f;
+float fl = -47.88f;
+
+double d = 6.3432224440003093d;
+double db = -4.344555533444d;
+
+char c = '*';
+char ch = '&';
+
+boolean bn = true;
+boolean bl = false;
+
+System.out.println(b + " , " + bt+" , "+s+" , "+sh+" , "+i+" , "+in+" , "+l+" , "+ln+" , "+f+" , "+
+	fl+" , "+d+" , "+db+" , "+ch+" , "+bn+" , "+bl);
+    }
+}
